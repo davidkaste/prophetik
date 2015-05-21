c7dbcacc669ae6b99274cd3ce6a8c85e6e9b4f44
davidkaste <d.castella.85@gmail.com>
2015-05-21 09:06:13 +0200

    Created bridge test between Cucumber and JUnit

---
+++ src/test/java/com/davidcastella/RunCukesTest.java
@@ -0,0 +1,11 @@
+package com.davidcastella;
+
+import org.junit.runner.RunWith;
+import cucumber.api.junit.Cucumber;
+
+/**
+ * Created by david on 20/05/15.
+ */
+
+@RunWith(Cucumber.class)
+public class RunCukesTest {}
