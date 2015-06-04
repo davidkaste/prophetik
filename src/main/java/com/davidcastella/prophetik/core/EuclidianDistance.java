ef90c5e0e98b6b7d4cfe234065728ed49acc382b
davidkaste <d.castella.85@gmail.com>
2015-06-04 13:55:40 +0200

    Added algorithms to compute user distance

---
+++ src/main/java/com/davidcastella/prophetik/core/EuclidianDistance.java
@@ -0,0 +1,12 @@
+package com.davidcastella.prophetik.core;
+
+import com.hp.hpl.jena.rdf.model.Model;
+
+/**
+ * Created by david on 03/06/15.
+ */
+public class EuclidianDistance implements UserDistance {
+    public double getUserDistance(Model graph, String user1Uri, String user2Uri) {
+        return 0;
+    }
+}
