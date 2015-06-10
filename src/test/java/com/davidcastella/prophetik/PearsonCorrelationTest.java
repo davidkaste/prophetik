164096e99a4664a999a3aa999de97ec3bcd55425
davidkaste <d.castella.85@gmail.com>
2015-06-10 20:42:21 +0200

    Moved UserDistance test implementations to core test package

--- src/test/java/com/davidcastella/prophetik/PearsonCorrelationTest.java
+++
@@ -1,7 +0,0 @@
-package com.davidcastella.prophetik;
-
-/**
- * Created by david_000 on 09/06/2015.
- */
-public class PearsonCorrelationTest {
-}
---
+++ src/test/java/com/davidcastella/prophetik/core/PearsonCorrelationTest.java
@@ -0,0 +1,28 @@
+package com.davidcastella.prophetik.core;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by david_000 on 10/06/2015.
+ */
+public class PearsonCorrelationTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetUserDistance() throws Exception {
+
+    }
+}
