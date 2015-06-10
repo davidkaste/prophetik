3b9ec935343f2b89aa27919c106d48aa16e305fc
davidkaste <d.castella.85@gmail.com>
2015-06-10 20:55:06 +0200

    Generated 1st version for UserDistance tests

--- src/test/java/com/davidcastella/prophetik/core/EuclidianDistanceTest.java
+++ src/test/java/com/davidcastella/prophetik/core/EuclidianDistanceTest.java
@@ -1,28 +1,30 @@
 package com.davidcastella.prophetik.core;
 
-import org.junit.After;
+import com.hp.hpl.jena.rdf.model.Model;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 /**
- * Created by david_000 on 10/06/2015.
+ * Created by David Castellà <david.castella@riseup.net> on 10/06/2015.
  */
 public class EuclidianDistanceTest {
 
+    private Model graphTest;
+    private UserDistance euclidian;
+
     @Before
     public void setUp() throws Exception {
-
-    }
-
-    @After
-    public void tearDown() throws Exception {
-
+        euclidian = new EuclidianDistance();
     }
 
     @Test
     public void testGetUserDistance() throws Exception {
-
+        String lisa = "http://testdomain.com/user/LisaRose";
+        String gene = "http://testdomain.com/user/GeneSeymour";
+        Double expected = 1.148148148148;
+        Double result = euclidian.getUserDistance(graphTest, lisa, gene);
+        assertEquals(expected, result);
     }
 }
