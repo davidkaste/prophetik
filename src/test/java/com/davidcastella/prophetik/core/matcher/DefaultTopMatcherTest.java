c52a63582b87d81a399e2b8aec60aec9ac44b361
davidkaste <david.castella@riseup.net>
2015-07-01 15:36:51 +0200

    Added DefaultTopMatcherTest

---
+++ src/test/java/com/davidcastella/prophetik/core/matcher/DefaultTopMatcherTest.java
@@ -0,0 +1,26 @@
+package com.davidcastella.prophetik.core.matcher;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by David on 25/06/2015.
+ */
+public class DefaultTopMatcherTest {
+
+    private TopMatcher m = new DefaultTopMatcher();
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testGetTopMatches() throws Exception {
+
+    }
+}
