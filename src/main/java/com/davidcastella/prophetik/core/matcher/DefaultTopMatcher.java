33ffbb9ac55ce904cbb16b1f82d20acc76ff3992
davidkaste <david.castella@riseup.net>
2015-07-01 15:36:03 +0200

    Created some packages for organize sources

--- src/main/java/com/davidcastella/prophetik/core/matcher/DefaultTopMatcher.java
+++ src/main/java/com/davidcastella/prophetik/core/matcher/DefaultTopMatcher.java
@@ -9,28 +9,28 @@ import java.util.Map;
 /**
  * Created by David Castellà on 14/06/2015.
  */
-public class Matcher {
+public class DefaultTopMatcher implements TopMatcher {
     private Model graph;
     private String userResourceUri;
     private int numberResults;
     private UserDistance distanceMethod;
 
-    public Matcher(Model graph, String userResourceUri, int numberResults, UserDistance distanceMethod) {
+    public DefaultTopMatcher(Model graph, String userResourceUri, int numberResults, UserDistance distanceMethod) {
         this.graph = graph;
         this.userResourceUri = userResourceUri;
         this.numberResults = numberResults;
         this.distanceMethod = distanceMethod;
     }
 
-    public Matcher(Model graph, String userResourceUri) {
+    public DefaultTopMatcher(Model graph, String userResourceUri) {
         this(graph, userResourceUri, 5, new PearsonCorrelation());
     }
 
-    public Matcher(Model graph, String userResourceUri, int numberResults) {
+    public DefaultTopMatcher(Model graph, String userResourceUri, int numberResults) {
         this(graph, userResourceUri, numberResults, new PearsonCorrelation());
     }
 
-    public Matcher(Model graph, String userResourceUri, UserDistance distanceMethod) {
+    public DefaultTopMatcher(Model graph, String userResourceUri, UserDistance distanceMethod) {
         this(graph, userResourceUri, 5, distanceMethod);
     }
 
---
+++ src/main/java/com/davidcastella/prophetik/core/matcher/TopMatcher.java
@@ -0,0 +1,11 @@
+package com.davidcastella.prophetik.core.matcher;
+
+import java.util.Map;
+
+/**
+ * Created by David on 01/07/2015.
+ */
+public interface TopMatcher {
+    public Map<String, Double> getTopMatches();
+    public Map<String, Double> getTopMatches(int numberResults);
+}
