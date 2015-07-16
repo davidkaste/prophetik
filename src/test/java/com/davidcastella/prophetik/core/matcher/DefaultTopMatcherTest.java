d5506e1e685811b54ee40752ed2cf2d07521068f
David Castellà <david.castella@riseup.net>
2015-07-16 14:20:05 +0200

    Updated DefaultTopMatcherTest

--- src/test/java/com/davidcastella/prophetik/core/matcher/DefaultTopMatcherTest.java
+++ src/test/java/com/davidcastella/prophetik/core/matcher/DefaultTopMatcherTest.java
@@ -1,18 +1,34 @@
 package com.davidcastella.prophetik.core.matcher;
 
+import com.davidcastella.prophetik.core.utils.Querier;
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by David on 25/06/2015.
  */
 public class DefaultTopMatcherTest {
 
-    private TopMatcher m = new DefaultTopMatcher();
+    private TopMatcher m;
+    private Model graphTest;
 
     @Before
     public void setUp() throws Exception {
+        OntModel auxGraph = ModelFactory.createOntologyModel();
+        graphTest = auxGraph.read(new InputStreamReader(
+                        new FileInputStream(getClass().getClassLoader().getResource("test-fixture.rdf").getPath())),
+                "RDF/XML-ABBREV");
+        m = new DefaultTopMatcher(graphTest, "http://www.testdomain.com/Toby", 3);
     }
 
     @After
@@ -21,6 +37,16 @@ public class DefaultTopMatcherTest {
 
     @Test
     public void testGetTopMatches() throws Exception {
+        Map<String, Double> result = m.getTopMatches();
+        result = Querier.getUserProducts(graphTest, "http://testdomain.com/user/LisaRose");
+        Assert.assertEquals(getExpectedResult(), result);
+    }
 
+    private Map<String, Double> getExpectedResult() {
+        Map<String, Double> expected = new HashMap<String, Double>();
+        expected.put("http://www.testdomain.com/LisaRose", 0.99124070716192991);
+        expected.put("http://www.testdomain.com/MickLaSalle", 0.92447345164190486);
+        expected.put("http://www.testdomain.com/ClaudiaPuig", 0.89340514744156474);
+        return expected;
     }
 }
