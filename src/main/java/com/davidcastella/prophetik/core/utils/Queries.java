dd7df9f34ec8c257dec8f069c2fa7e4cec606ddb
David Castellà <david.castella@riseup.net>
2015-07-20 00:56:51 +0200

    Added Queries class

---
+++ src/main/java/com/davidcastella/prophetik/core/utils/Queries.java
@@ -0,0 +1,28 @@
+package com.davidcastella.prophetik.core.utils;
+
+/**
+ * Created by davidkaste on 19/7/15.
+ */
+public class Queries {
+
+    public static String getUserProductsRating(String userResource) {
+        return "prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                "prefix schema: <http://schema.org/>\n" +
+                "select ?product ?ratingValue where {\n" +
+                "    ?review schema:author <" + userResource + ">.\n" +
+                "    ?review schema:itemReviewed ?product.\n" +
+                "    ?review schema:reviewRating ?rating.\n" +
+                "    ?rating schema:ratingValue ?ratingValue\n" +
+                "}";
+    }
+
+    public static String getAllUsers(String userResource) {
+        return "prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                "prefix schema: <http://schema.org/>\n" +
+                "SELECT ?user\n" +
+                "WHERE {\n" +
+                "  ?user rdf:type schema:Person\n" +
+                "  FILTER(str(?user) !=\"" + userResource + "\")\n" +
+                "}";
+    }
+}
