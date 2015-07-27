4eb2312a2fc5070eb378fde4e139420af0bc1c3f
David Castellà <david.castella@riseup.net>
2015-07-27 23:56:03 +0200

    Implemented default recommender engine

---
+++ src/main/java/com/davidcastella/prophetik/core/engine/DefaultRecommenderEngine.java
@@ -0,0 +1,68 @@
+package com.davidcastella.prophetik.core.engine;
+
+import com.davidcastella.prophetik.core.userdistance.UserDistance;
+import com.davidcastella.prophetik.core.utils.Querier;
+import com.hp.hpl.jena.rdf.model.Model;
+
+import java.util.*;
+
+/**
+ * Created by davidkaste on 21/7/15.
+ */
+public class DefaultRecommenderEngine implements RecommenderEngine {
+
+    public Map<String, Double> getRecommendations(Model graph, String userResource, UserDistance simMethod) {
+        Map<String, Double> totals = new HashMap<String, Double>();
+        Map<String, Double> simSums = new HashMap<String, Double>();
+        Map<String, Double> user1Ratings, user2Ratings;
+        Map<Double, String> rankings = new HashMap<Double, String>();
+        Map<String, Double> rankReturn = new HashMap<String, Double>();
+
+        List<String> userList = Querier.getAllUsers(graph, userResource);
+        List<Double> rankingsIndex = null;
+
+        Double sim;
+
+        user1Ratings = Querier.getUserProductsRatings(graph, userResource);
+
+        for(String user : userList) {
+            sim = simMethod.getUserDistance(graph, userResource, user);
+
+            user2Ratings = Querier.getUserProductsRatings(graph, user);
+
+            if(sim > 0) {
+
+                for(String product : user2Ratings.keySet()) {
+                    if(!user1Ratings.containsKey(product) || user1Ratings.get(product) == 0.0) {
+                        if(!totals.containsKey(product)) {
+                            totals.put(product, 0.0);
+                            simSums.put(product, 0.0);
+                        }
+                        totals.put(product, totals.get(product) + (user2Ratings.get(product) * sim));
+                        simSums.put(product, simSums.get(product) + sim);
+                    }
+                }
+            }
+        }
+
+        Double rank = 0.0;
+        Double totalsValue = 0.0;
+        Double simSumsValue = 0.0;
+        for(String key : totals.keySet()) {
+            totalsValue = totals.get(key);
+            simSumsValue = simSums.get(key);
+            rank = totalsValue / simSumsValue;
+            rankings.put(rank, key);
+        }
+
+        rankingsIndex = new ArrayList<Double>(rankings.keySet());
+        Collections.sort(rankingsIndex);
+        Collections.reverse(rankingsIndex);
+
+        for(Double key : rankingsIndex) {
+            rankReturn.put(rankings.get(key), key);
+        }
+
+        return rankReturn;
+    }
+}
